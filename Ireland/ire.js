e876969bd20a61282ab5c575dd0d0d020fc4db48
javstudent40 <titusgr30@yahoo.com>
2018-07-12 19:51:45 -0400

    new clone

--- Ireland/ire.js
+++
@@ -1,2 +0,0 @@
-// Making Images Expand with Click
-
---
+++ ire.js
@@ -0,0 +1,8 @@
+// Clicking on picture logs 'Clicked' to javascript console
+
+function change() {
+	var highlight = document.getElementsByTagName('img');
+	console.log('clicked');
+}
+
+change();

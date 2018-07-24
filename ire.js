338cd0089a4de50d671f368d69a9fd6a4825cd3a
javstudent40 <titusgr30@yahoo.com>
2018-07-24 18:19:20 -0400

    added new javascript file

--- ire.js
+++ ire.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Clicking on picture logs 'Clicked' to javascript console
 
 function change() {
@@ -6,3 +7,23 @@ function change() {
 }
 
 change();
+=======
+// Clicking on button toggles registration form as well as changing text within button
+
+var x = document.getElementById("thisDiv");
+var y = document.querySelector("button");
+
+
+function reg() {
+	
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		y.innerHTML = "Register Now!"
+	} else {
+		x.style.display = "none";
+		y.innerHTML = "Are you sure?";
+	}
+	
+};
+
+>>>>>>> Changed javascript to toggle registration form
